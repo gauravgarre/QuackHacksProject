d822ed42f48932de5176f6f2ac7c2a874f5da31f
RRR <rahikotadia@gmail.com>
2020-09-11 20:36:34 -0700

    RecyclerView

--- app/src/main/java/com/example/quackhacksproject/MainActivity.java
+++ app/src/main/java/com/example/quackhacksproject/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.quackhacksproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView classRecyclerView;
+    String s1[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        classRecyclerView = findViewById(R.id.classRecyclerView);
+
+
+        MyAdapter myAdapter = new MyAdapter(this, s1);
+
     }
     public void rayTest() {
 
