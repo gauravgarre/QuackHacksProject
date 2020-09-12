404b5c43c4e2ff4686d196e2c46b313d54bb0060
rayli1639 <ray.z.li@gmail.com>
2020-09-11 23:41:51 -0400

    Added switch to registration page

--- app/src/main/java/com/example/quackhacksproject/RegistrationActivity.java
+++ app/src/main/java/com/example/quackhacksproject/RegistrationActivity.java
@@ -3,6 +3,10 @@ package com.example.quackhacksproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        final Switch teacherStudentSwitch = findViewById(R.id.teacherStudentSwitch);
+        final TextView teacherStudentText = findViewById(R.id.teacherStudentText);
+        teacherStudentSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void  onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                if (isChecked){
+                    teacherStudentText.setText("Logging in as student");
+                }
+                else{
+                    teacherStudentText.setText("Logging in as teacher");
+                }
+            }
+
+        });
     }
 }
